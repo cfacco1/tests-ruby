cb129a5649a2978233588736b44385b21c0ba838
cfacco1 <facco.clement@gmail.com>
2020-04-13 17:16:55 +0200

    Ajout 4

---
+++ lib/04_simon_says.rb
@@ -0,0 +1,23 @@
+def echo (string)
+  return string
+end
+
+def shout (string)
+  string.upcase
+end
+
+def repeat
+end
+
+def start_of_word(string, n)
+  return string[0...n]
+end
+
+def first_word(string)
+   return string.split[0]
+end
+
+#La méthode split() permet de diviser une chaîne de caractères à partir d'un séparateur pour fournir un tableau de sous-chaînes.
+
+def titleize
+end
